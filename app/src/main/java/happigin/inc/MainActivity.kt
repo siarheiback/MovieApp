708e389f0ac5af47f2d9bc518150842cedcfe1ef
siarheiback <sergeibavtrukevich@gmail.com>
2022-12-16 17:44:09 +0300

    simple retrofit error hadle

--- app/src/main/java/happigin/inc/MainActivity.kt
+++ app/src/main/java/happigin/inc/MainActivity.kt
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import happigin.inc.databinding.ActivityMainBinding
 import happigin.inc.presentation.RecyclerViewAdapter
-import happigin.inc.retrofit.ApiService
-import happigin.inc.retrofit.RetrofitHelper
+import happigin.inc.retrofit.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,13 +27,29 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recycler.adapter = adapter
         binding.button.setOnClickListener(){
+            
             // launching a new coroutine
             GlobalScope.launch(Dispatchers.Main) {
                 binding.progressBar.visibility= View.VISIBLE
-                val result = quotesApi.getMovieByKey(binding.searchText.text.toString(), 1)
-                adapter.differ.submitList(result.body()?.films)
-                Log.d("TAG ", result.body().toString())
-                binding.progressBar.visibility= View.GONE
+                try {
+                    val result = quotesApi.getMovieByKey(binding.searchText.text.toString(), 1)
+                    if (result.isSuccessful) {
+                        adapter.differ.submitList(result.body()?.films)
+                        Toast.makeText(this@MainActivity,"ok",Toast.LENGTH_SHORT).show()
+                    } else {
+                        throw IOException(result.code().toString())
+                    }
+                    Log.d("TAG ", result.toString())
+                    binding.progressBar.visibility = View.GONE
+                }
+                catch (e: HttpException) {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_SHORT).show()
+                }catch (e: IOException){
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_SHORT).show()
+                }
+
             }
         }
 
